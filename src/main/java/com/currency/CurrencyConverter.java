2d01818a242dd3c1d1f8023e342a80c9bceda0c0
aajay88287-maker <aajay88287@gmail.com>
2026-05-08 10:35:23 +0530

    Added Jenkinsfile

--- src/main/java/com/currency/CurrencyConverter.java
+++ src/main/java/com/currency/CurrencyConverter.java
@@ -2,4 +2,37 @@ package com.currency;
 
 public class CurrencyConverter {
 
+    static double USD_TO_INR = 83.0;
+    static double EUR_TO_INR = 90.0;
+    static double USD_TO_EUR = 0.92;
+
+    // Rupee to Dollar
+    public double rupeeToDollar(double rupee) {
+        return rupee / USD_TO_INR;
+    }
+
+    // Dollar to Rupee
+    public double dollarToRupee(double dollar) {
+        return dollar * USD_TO_INR;
+    }
+
+    // Rupee to Euro
+    public double rupeeToEuro(double rupee) {
+        return rupee / EUR_TO_INR;
+    }
+
+    // Euro to Rupee
+    public double euroToRupee(double euro) {
+        return euro * EUR_TO_INR;
+    }
+
+    // Dollar to Euro
+    public double dollarToEuro(double dollar) {
+        return dollar * USD_TO_EUR;
+    }
+
+    // Euro to Dollar
+    public double euroToDollar(double euro) {
+        return euro / USD_TO_EUR;
+    }
 }
--- src/test/java/com/currency/CurrencyCoverterTest.java
+++ src/test/java/com/currency/CurrencyCoverterTest.java
@@ -1,5 +1,39 @@
 package com.currency;
 
-public class CurrencyCoverterTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
+public class CurrencyConverterTest {
+
+    CurrencyConverter cc = new CurrencyConverter();
+
+    @Test
+    public void testRupeeToDollar() {
+        assertEquals(1.0, cc.rupeeToDollar(83), 0.01);
+    }
+
+    @Test
+    public void testDollarToRupee() {
+        assertEquals(83.0, cc.dollarToRupee(1), 0.01);
+    }
+
+    @Test
+    public void testRupeeToEuro() {
+        assertEquals(1.0, cc.rupeeToEuro(90), 0.01);
+    }
+
+    @Test
+    public void testEuroToRupee() {
+        assertEquals(90.0, cc.euroToRupee(1), 0.01);
+    }
+
+    @Test
+    public void testDollarToEuro() {
+        assertEquals(0.92, cc.dollarToEuro(1), 0.01);
+    }
+
+    @Test
+    public void testEuroToDollar() {
+        assertEquals(1.08, cc.euroToDollar(1), 0.01);
+    }
 }
